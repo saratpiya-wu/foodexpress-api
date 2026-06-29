93fd8ff3b6e0a4014d746bb9292f2550332e24ac
PIYA-SARAT <piya-sarat@mptc.gov.kh>
2026-06-29 13:47:26 +0700

    Open port 8080 for Jenkins webhook

--- terraform/main.tf
+++ terraform/main.tf
@@ -54,6 +54,13 @@ resource "aws_security_group" "foodexpress_sg" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  ingress {
+  description = "Jenkins"
+  from_port   = 8080
+  to_port     = 8080
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+}
 }
 
 resource "aws_instance" "foodexpress_server" {
